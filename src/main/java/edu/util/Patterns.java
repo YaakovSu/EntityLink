f8afe803e4c28f7ee8dcbfdf641caedcaee25c5e
SuNoob <yhsu1224@163.com>
2016-01-22 20:02:50 +0800

    '增加的抽取维基文本的代码'

--- src/main/java/edu/util/Patterns.java
+++ src/main/java/edu/util/Patterns.java
@@ -5,23 +5,23 @@ package edu.util;
  */
 public class Patterns {
     //从文本中抽取歧义页面的patterns
-    public static final String pattern1_1 = "(\\*\\s\\[{2})(.*?)(\\]{2})"; //* [[]]
-    public static final String pattern1_2 = "(\\*\\[{2})(.*?)(\\]{2})"; //*[[]]
-    public static final String pattern2_1 = "(\\*{2}\\[{2})(.*?)(\\]{2})"; //**[[]]
-    public static final String pattern2_2 = "(\\*{2}\\s\\[{2})(.*?)(\\]{2})"; //**[[]]
-    public static final String pattern3_1 = "(\\*'''\\[{2})(.*?)(\\]{2}''')";//*'''[[]]'''
-    public static final String pattern4_1 = "('''\\[{2})(.*?)(\\]{2}''')";//'''[[]]'''
-    public static final String pattern4_2 = "('''\\s\\[{2})(.*?)(\\]{2}''')";//'''[[]]'''
-    public static final String pattern5_1 = "(\\#\\[{2})(.*?)(\\]{2})";//#[[]]
-    public static final String pattern5_2 = "(\\#\\s\\[{2})(.*?)(\\]{2})";//#[[]]
-
-    public static final String pattern6_1 = "(见\\[{2})(.*?)(\\]{2})"; //见[[]]
-    public static final String pattern6_2 = "(見\\[{2})(.*?)(\\]{2})"; //見[[]]
-    public static final String pattern7_1 = "(\\*[u4E00-u9FA5]\\[{2})(.*?)(\\]{2})"; //*汉字[[]]
-    public static final String pattern7_2 = "(：\\[{2})(.*?)(\\]{2})"; //*汉字：[[]]
-
-    public static final String pattern8_1 = "(又称\\[{2})(.*?)(\\]{2})"; //又称[[]]
-    public static final String pattern8_2 = "(又稱\\[{2})(.*?)(\\]{2})"; //又稱[[]]
+//    public static final String pattern1_1 = "(\\*\\s\\[{2})(.*?)(\\]{2})"; //* [[]]
+//    public static final String pattern1_2 = "(\\*\\[{2})(.*?)(\\]{2})"; //*[[]]
+//    public static final String pattern2_1 = "(\\*{2}\\[{2})(.*?)(\\]{2})"; //**[[]]
+//    public static final String pattern2_2 = "(\\*{2}\\s\\[{2})(.*?)(\\]{2})"; //**[[]]
+//    public static final String pattern3_1 = "(\\*'''\\[{2})(.*?)(\\]{2}''')";//*'''[[]]'''
+//    public static final String pattern4_1 = "('''\\[{2})(.*?)(\\]{2}''')";//'''[[]]'''
+//    public static final String pattern4_2 = "('''\\s\\[{2})(.*?)(\\]{2}''')";//'''[[]]'''
+//    public static final String pattern5_1 = "(\\#\\[{2})(.*?)(\\]{2})";//#[[]]
+//    public static final String pattern5_2 = "(\\#\\s\\[{2})(.*?)(\\]{2})";//#[[]]
+//
+//    public static final String pattern6_1 = "(见\\[{2})(.*?)(\\]{2})"; //见[[]]
+//    public static final String pattern6_2 = "(見\\[{2})(.*?)(\\]{2})"; //見[[]]
+//    public static final String pattern7_1 = "(\\*[u4E00-u9FA5]\\[{2})(.*?)(\\]{2})"; //*汉字[[]]
+//    public static final String pattern7_2 = "(：\\[{2})(.*?)(\\]{2})"; //*汉字：[[]]
+//
+//    public static final String pattern8_1 = "(又称\\[{2})(.*?)(\\]{2})"; //又称[[]]
+//    public static final String pattern8_2 = "(又稱\\[{2})(.*?)(\\]{2})"; //又稱[[]]
 
     //只取中文文本
     public static final String pattern_Zh = "([\u0000-\u007F])"; //匹配中文
--- src/main/java/mytools/generateDisambigationMap.java
+++ src/main/java/mytools/generateDisambigationMap.java
@@ -1,96 +1,96 @@
-package mytools;
-
-import edu.util.Contant;
-import edu.util.opMysql;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static edu.util.Contant.*;
-import static edu.util.Patterns.*;
-
-/**
- * 针对Page表，抽取出含有消歧义标识的词条，并利用pattern抽取出维基百科中所有歧义词条生成一个hashmap
- * Created by youngsu on 14-10-27.
- */
-public class generateDisambigationMap {
-    public static HashMap<String, ArrayList<String>> getAllDisambigationTitle() throws SQLException {
-
-        Connection conn = opMysql.connSQL(Contant.zhmysqlurl);
-        ResultSet resultSet = opMysql.selectSQL(conn, selectWikiDisAmbiguation); //查询出来的已经是处理好只有消歧义的词条的
-        HashMap<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
-        int count = 0;
-        while (resultSet.next()) {
-            count++;
-            String name = resultSet.getString("name");
-            String text = resultSet.getString("text");
-            System.out.println("已经执行了：" + count);
-            result = extractOneDisambigationTitle(result, name, text);
-
-        }
-        return result;
-    }
-
-    //从一条记录的文本中抽取出该title的消歧义页面的titile
-    private static HashMap<String, ArrayList<String>> extractOneDisambigationTitle(HashMap<String, ArrayList<String>> result, String name, String text) {
-        String key = name.split("_")[0];
-        ArrayList<String> value = cleanText(text);
-//        value.add(text);
-        if ((!result.containsKey(key))) {
-            result.put(key, value);
-        } else if (result.get(key).size() == 0) {
-            result.put(key, value);
-        }
-
-        return result;
-    }
-
-    private static ArrayList<String> cleanText(String text) {
-//        System.out.println(text+"  first");
-        String[] patterns = {pattern1_1, pattern1_2, pattern2_1, pattern2_2,
-                pattern3_1, pattern4_1, pattern4_2, pattern5_1, pattern5_2, pattern6_1, pattern6_2, pattern7_1,
-                pattern7_2, pattern8_1, pattern8_2};
-        ArrayList<String> result = new ArrayList<String>();
-        for (String pattern : patterns) {
-            Matcher m = Pattern.compile(pattern).matcher(text);
-            while (m.find()) {
-                String testContant = m.group(2);
-//                System.out.println(testContant+"  first");
-                result.add(testContant);
-//                System.out.println(testContant);
-            }
-        }
-
-
-        return result;
-    }
-
-    public static void main(String[] args) throws SQLException, IOException {
-        FileWriter fw1 = new FileWriter(disambigationSemi_Output_Path);
-        FileWriter fw2 = new FileWriter(disambigationOutput_Path);
-        HashMap<String, ArrayList<String>> result = getAllDisambigationTitle();
-        Iterator<String> iterator = result.keySet().iterator();
-        while (iterator.hasNext()) {
-            String name = iterator.next();
-            ArrayList<String> value = result.get(name);
-            fw1.write(name + "\t" + value);
-            fw1.write("\n");
-            if (!value.isEmpty()) {
-                fw2.write(name + "\t" + value);
-                fw2.write("\n");
-            }
-            System.out.println(name + "\t" + value);
-        }
-        fw1.close();
-        fw2.close();
-
-    }
-}
+//package mytools;
+//
+//import edu.util.Contant;
+//import edu.util.opMysql;
+//
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//import static edu.util.Contant.*;
+//import static edu.util.Patterns.*;
+//
+///**
+// * 针对Page表，抽取出含有消歧义标识的词条，并利用pattern抽取出维基百科中所有歧义词条生成一个hashmap
+// * Created by youngsu on 14-10-27.
+// */
+//public class generateDisambigationMap {
+//    public static HashMap<String, ArrayList<String>> getAllDisambigationTitle() throws SQLException {
+//
+//        Connection conn = opMysql.connSQL(Contant.zhmysqlurl);
+//        ResultSet resultSet = opMysql.selectSQL(conn, selectWikiDisAmbiguation); //查询出来的已经是处理好只有消歧义的词条的
+//        HashMap<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
+//        int count = 0;
+//        while (resultSet.next()) {
+//            count++;
+//            String name = resultSet.getString("name");
+//            String text = resultSet.getString("text");
+//            System.out.println("已经执行了：" + count);
+//            result = extractOneDisambigationTitle(result, name, text);
+//
+//        }
+//        return result;
+//    }
+//
+//    //从一条记录的文本中抽取出该title的消歧义页面的titile
+//    private static HashMap<String, ArrayList<String>> extractOneDisambigationTitle(HashMap<String, ArrayList<String>> result, String name, String text) {
+//        String key = name.split("_")[0];
+//        ArrayList<String> value = cleanText(text);
+////        value.add(text);
+//        if ((!result.containsKey(key))) {
+//            result.put(key, value);
+//        } else if (result.get(key).size() == 0) {
+//            result.put(key, value);
+//        }
+//
+//        return result;
+//    }
+//
+//    private static ArrayList<String> cleanText(String text) {
+////        System.out.println(text+"  first");
+//        String[] patterns = {pattern1_1, pattern1_2, pattern2_1, pattern2_2,
+//                pattern3_1, pattern4_1, pattern4_2, pattern5_1, pattern5_2, pattern6_1, pattern6_2, pattern7_1,
+//                pattern7_2, pattern8_1, pattern8_2};
+//        ArrayList<String> result = new ArrayList<String>();
+//        for (String pattern : patterns) {
+//            Matcher m = Pattern.compile(pattern).matcher(text);
+//            while (m.find()) {
+//                String testContant = m.group(2);
+////                System.out.println(testContant+"  first");
+//                result.add(testContant);
+////                System.out.println(testContant);
+//            }
+//        }
+//
+//
+//        return result;
+//    }
+//
+//    public static void main(String[] args) throws SQLException, IOException {
+//        FileWriter fw1 = new FileWriter(disambigationSemi_Output_Path);
+//        FileWriter fw2 = new FileWriter(disambigationOutput_Path);
+//        HashMap<String, ArrayList<String>> result = getAllDisambigationTitle();
+//        Iterator<String> iterator = result.keySet().iterator();
+//        while (iterator.hasNext()) {
+//            String name = iterator.next();
+//            ArrayList<String> value = result.get(name);
+//            fw1.write(name + "\t" + value);
+//            fw1.write("\n");
+//            if (!value.isEmpty()) {
+//                fw2.write(name + "\t" + value);
+//                fw2.write("\n");
+//            }
+//            System.out.println(name + "\t" + value);
+//        }
+//        fw1.close();
+//        fw2.close();
+//
+//    }
+//}
